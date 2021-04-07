6c85b242c04dcc50556e587a3ce195f94952fd6d
kenewstar <kenewstar@163.com>
2021-04-07 22:52:59 +0800

    [MOD]添加注释

--- src/main/java/org/kenewstar/jdbc/core/ConditionJdbcExecutor.java
+++ src/main/java/org/kenewstar/jdbc/core/ConditionJdbcExecutor.java
@@ -21,7 +21,13 @@ public interface ConditionJdbcExecutor {
      */
     <T> List<T> selectList(Class<?> fromClass, Class<T> resultType, MapTo mapTo);
 
-
+    /**
+     * 单表条件查询
+     * @param entityClass 实体类
+     * @param mapTo 条件SQL构造对象
+     * @param <T> t
+     * @return list
+     */
     <T> List<T> selectList(Class<T> entityClass, MapTo mapTo);
 
 }
